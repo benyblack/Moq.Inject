2392a94eb46700e80955e9fbb5608fb6a61391b9
Behnam <benyblack@gmail.com>
2021-07-21 10:51:33 +0200

    Add documentation

--- src/Moq.Inject/Injector_1.cs
+++ src/Moq.Inject/Injector_1.cs
@@ -12,12 +12,23 @@ namespace Moq.Inject
             Inputs = new Dictionary<string, object>();
         }
 
+        /// <summary>
+        /// Adds a parameter to the constructor of the object to be instantiated
+        /// </summary>
+        /// <param name="name">Name of the parameter in the constructor. This is case sensitive.</param>
+        /// <param name="value">The value to be used for the parameter</param>
+        /// <returns></returns>
         public Injector<T> Add(string name, Object value)
         {
             Inputs.Add(name, value);
             return this;
         }
 
+        /// <summary>
+        /// Create an instance of an object with the given constructor parameters.
+        /// It uses a mocked object for a mockable or a default value for the others if a parameter is not provided.
+        /// </summary>
+        /// <returns></returns>
         public T Create()
         {
             return Injector.Create<T>(Inputs);
